11964b1c183b20c26c983bef34227d37c0b51c9b
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-08-31 12:24:30 -0700

    adv task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #Linda Adisa
-def complex_delete(a_dictionary, value):
-    for i, j in a_dictionary.items():
-        if j == value:
-            del a_dictionary[i]
-            return complex_delete(a_dictionary, value)
-        return a_dictionary
+def complex_delete(my_dict, value):
+    keys_to_del = []
+    for key in my_dict:
+        if my_dict[key] == value:
+            keys_to_del.append(key)
+            for key in keys_to_del:
+                del my_dict[key]
+                return my_dict
