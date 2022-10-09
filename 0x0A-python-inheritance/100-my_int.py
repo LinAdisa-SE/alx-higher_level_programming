422bb76c551bc589901b14240300e382d76e69c5
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-10-09 03:31:53 -0700

    adv task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,10 @@
- #!/usr/bin/python3
- """ New class """
- class MyInt(int):
-     """ My int inherits from int """
-     def __eq__(self, num):
-         """ Function for equals """
-         return(int(self) != int(num))
-     def __ne__(self, num):
-         """ Function for not equals """
-         return (int(self) == int(num))
+#!/usr/bin/python3
+""" New class """
+class MyInt(int):
+    """ My int inherits from int """
+    def __eq__(self, num):
+        """ Function for equals """
+        return(int(self) != int(num))
+    def __ne__(self, num):
+        """ Function for not equals """
+        return (int(self) == int(num))
